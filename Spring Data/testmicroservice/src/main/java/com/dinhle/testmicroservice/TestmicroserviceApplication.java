b1261063a9c9eadb932816da24c90adb9e1460c2
dinh-letat <dinhprococoa@gmail.com>
2024-03-25 17:49:19 +0700

    init testmicroservice

---
+++ Spring Data/testmicroservice/src/main/java/com/dinhle/testmicroservice/TestmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.dinhle.testmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestmicroserviceApplication.class, args);
+	}
+
+}
